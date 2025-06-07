238cee2970d7471cb9d278669d52ee348fd2bda1
WhiteSteps <simo.nole12@gmail.com>
2025-06-07 16:14:25 +0200

    implemented BitMap_getBytes

--- source/bit_map.c
+++ source/bit_map.c
@@ -1,4 +1,22 @@
 #include <assert.h>
-#include <stdio.h>
+#include "../headers/bit_map.h"
 
-//working in progress
+// returns the number of bytes to store bits booleans
+int BitMap_getBytes(int bits){
+  int bytes = bits/8;
+  if (bits%8) return bytes+1; //if there are any bits left, then I add an extra byte
+  return bytes;
+}
+
+// initializes a bitmap on an external array
+void BitMap_init(BitMap* bit_map, int num_bits, uint8_t* buffer);
+
+// sets a the bit bit_num in the bitmap
+// status= 0 or 1
+void BitMap_setBit(BitMap* bit_map, int bit_num, int status);
+
+// inspects the status of the bit bit_num
+int BitMap_bit(const BitMap* bit_map, int bit_num);
+
+// print the entire bitmap
+void BitMap_print(BitMap* bit_map);
