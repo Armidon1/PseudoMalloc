e4fb3c7f87528842f89fe291872ae8453f15f539
WhiteSteps <simo.nole12@gmail.com>
2025-06-07 18:04:57 +0200

    testing BitMap_init and BitMap_print

--- test/test_bitmap.c
+++ test/test_bitmap.c
@@ -3,13 +3,18 @@
 #include "../headers/bit_map.h"
 
 int main(){
+    //creating a bitmap
     int num_bits = 8; //just one byte
     int buffer_size = BitMap_getBytes(num_bits);
     uint8_t buffer[buffer_size];
     BitMap bm;
-    BitMap_init(&bm, num_bits, &buffer);
+    BitMap_init(&bm, num_bits, buffer);
     printf("created a Bitmap with num_bits = %d\n", num_bits);
+
+    //testing bitmap_print
     printf("testing BitMap_print\n");
     BitMap_print(&bm);
+
+    
     return 0;
 }
